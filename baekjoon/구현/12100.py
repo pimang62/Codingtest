92e74ffec4a9c2c445e6ac548ecc6e8ed33a5f90
pimang62 <pimang62@gmail.com>
2023-12-14 00:16:26 +0900

    12/14

--- baekjoon/구현/12100.py
+++ baekjoon/구현/12100.py
@@ -173,109 +173,64 @@ print(bfs())
 # print(b_prime)
 
 # [다른 사람의 풀이]
-# 출처 : https://www.acmicpc.net/board/view/113931
-import sys
-from copy import deepcopy
-input = sys.stdin.readline
-global N
-global _map
-global perm_array
-global my_max
-my_max =[]
-N = int(input())
-_map = [list(map(int, input().split())) for _ in range(N)]
-perm_array = []
+# 출처 : https://jeongchul.tistory.com/667
+from collections import deque
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+answer, q = 0, deque()
 
+def get(i, j):
+    if board[i][j]: # 0이 아닌 값이라면
+        q.append(board[i][j]) # queue에 board의 값을 넣는다.
+        board[i][j] = 0 # 처리가 된 빈 자리는 0으로 값 업데이트
+        
+def merge(i, j, di, dj): # row index, column index, y방향, x방향 
+    while q:
+        x = q.popleft() # 움직이려는 블록 값을 가져온다. FIFO 
+        if not board[i][j]: # 0이라면 그대로 놓는다.
+            board[i][j] = x
+        elif board[i][j] == x: # 값이 일치한다면
+            board[i][j] = x*2 # 합쳐지므로 2배로 증가
+            i, j = i+di, j+dj 
+        else: # 값이 일치하지 않으면
+            i, j = i+di, j+dj
+            board[i][j] = x 
 
-def my_perm(cnt, arr):
-    global N
-    global _map
-    global my_max
-    if cnt == 5:
-        my_map = [_m[:] for _m in _map[:]]
-        # 0 상 1하 2좌 3우
-        for dir in arr:
-            go(my_map, N, dir)
-#-------------------------------------------------------------------
-        #my_max = max(max(max(my_map)), my_max)
-        my_max.append(max(map(max,my_map)))
-#-------------------------------------------------------------------
+def move(k):
+    # board[i][j]
+    if k == 0: # 위로 이동, 블락들이 위로 모두 이동하면 row index는 0
+        for j in range(n):
+            for i in range(n):
+                get(i, j)
+            merge(0, j, 1, 0) # row index 1씩 증가하면서 아래쪽 블락들을 합쳐감
+    elif k == 1: # 아래로 이동, 블락들이 아래로 모두 이동하면 row index는 n-1
+        for j in range(n):
+            for i in range(n-1, -1, -1):
+                get(i, j)
+            merge(n-1, j, -1, 0) # row 인덱스 1씩 감소하면서 위쪽들을 합쳐감
+    elif k == 2: # 오른쪽으로 이동, column index는 0
+        for i in range(n):
+            for j in range(n):
+                get(i, j)
+            merge(i, 0, 0, 1) # column 인덱스 증가 오른쪽으로 이동
+    else: # 왼쪽으로 이동, column index는 n-1
+        for i in range(n):
+            for j in range(n-1, -1, -1):
+                get(i, j)
+            merge(i, n-1, 0, -1) # column 인덱스 감소 왼쪽으로 이동
+            
+def solve(count):
+    global board, answer
+    if count == 5: # 최대 5번까지 움직였다면
+        for i in range(n):
+            answer = max(answer, max(board[i])) # 가장 큰 값이 answer
         return
+    b = [x[:] for x in board] # 방향을 바꾸기 전에 원래의 보드를 기억해야 한다.
+    
+    for k in range(4): # 4방향으로 움직인다.
+        move(k) # 움직인다.
+        solve(count+1) # 재귀적으로 호출한다.
+        board = [x[:] for x in b]
 
-
-    for i in range(4):
-        my_perm(cnt + 1, arr + [i])
-
-
-# dir 0 상 1 하 2 좌 3 우
-def go(board, n, dir):
-    if dir == 0:
-        for j in range(n):
-            pointer = 0
-            for i in range(1, n):
-                if board[i][j]:
-                    tmp = board[i][j]
-                    board[i][j] = 0
-                    # 포인터가 가리키는 수가 0일 때
-                    if board[pointer][j] == 0:
-                        board[pointer][j] = tmp
-                    # 포인터가 가리키는 수와 현재 위치의 수가 같을 때
-                    elif board[pointer][j] == tmp:
-                        board[pointer][j] *= 2
-                        pointer += 1
-                    # 포인터가 가리키는 수와 현재 위치의 수가 다를 때
-                    else:
-                        pointer += 1
-                        board[pointer][j] = tmp
-    if dir == 1:
-
-        for j in range(n):
-            pointer = n - 1
-            for i in range(n - 2, -1, -1):
-                if board[i][j]:
-                    tmp = board[i][j]
-                    board[i][j] = 0
-                    if board[pointer][j] == 0:
-                        board[pointer][j] = tmp
-                    elif board[pointer][j] == tmp:
-                        board[pointer][j] *= 2
-                        pointer -= 1
-                    else:
-                        pointer -= 1
-                        board[pointer][j] = tmp
-    if dir == 2:
-
-        for i in range(n):
-            pointer = 0
-            for j in range(1, n):
-                if board[i][j]:
-                    tmp = board[i][j]
-                    board[i][j] = 0
-                    if board[i][pointer] == 0:
-                        board[i][pointer] = tmp
-                    elif board[i][pointer] == tmp:
-                        board[i][pointer] *= 2
-                        pointer += 1
-                    else:
-                        pointer += 1
-                        board[i][pointer] = tmp
-    if dir == 3:
-
-        for i in range(n):
-            pointer = n - 1
-            for j in range(n - 2, -1, -1):
-                if board[i][j]:
-                    tmp = board[i][j]
-                    board[i][j] = 0
-                    if board[i][pointer] == 0:
-                        board[i][pointer] = tmp
-                    elif board[i][pointer] == tmp:
-                        board[i][pointer] *= 2
-                        pointer -= 1
-                    else:
-                        pointer -= 1
-                        board[i][pointer] = tmp
-
-
-my_perm(0, [])
-print(max(my_max))
+solve(0)
+print(answer)
