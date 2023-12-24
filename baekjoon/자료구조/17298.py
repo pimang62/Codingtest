4db4a7576503441737a573d3fde5a386b87d02ac
pimang62 <pimang62@gmail.com>
2023-12-24 15:12:00 +0900

    12/24

--- baekjoon/자료구조/17298.py
+++ baekjoon/자료구조/17298.py
@@ -15,7 +15,6 @@ list = [-1  8  8  -1]
 - A.pop() 원소가 max_val보다 크면 넘김(-1) max_val 갱신
 - A.pop() 원소가 max_val보다 작으면 max_val
 
-
 '''
 n = int(input())
 A = list(map(int, input().split()))
@@ -24,13 +23,13 @@ d = [-1]*n
 stack = []
 
 for i in range(n-1, -1, -1):
-    if not stack:
-        stack.append((A[i]))  # index 필요 없음
-        continue
+    # 나보다 작거나 같은 애들은 다 없애고
     while stack and stack[-1] <= A[i]:
         stack.pop()
-    if stack:  # 큰 값이 남아 있으면
-        d[i] = stack[-1]
+    # 큰 값이 남아 있으면
+    if stack and stack[-1] > A[i]:
+        d[i] = stack[-1]  # 기록
+    # 큰 값이 없다면 그냥 넘어감 (-1)
     stack.append(A[i])  # 담기만 함
 
 print(*d)
