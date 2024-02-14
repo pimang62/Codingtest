b22bf08f0e6418bdca36bcaa1b04f4207239e7cb
pimang62 <pimang62@gmail.com>
2024-02-15 00:52:49 +0900

    2/14

--- baekjoon/구현/1913.py
+++ baekjoon/구현/1913.py
@@ -37,57 +37,61 @@ target = int(input())
 graph = [[0]*n for _ in range(n)]
 graph[n//2][n//2] = 1  # initialize
 
-# target과 같을 때
-answer = [n//2, n//2]  # initialize 1!!
+# # target과 같을 때
+# answer = [n//2, n//2]  # initialize 1!!
 
-def check(x, y):
-    global answer
-    if graph[x][y] == target:
-        answer = [x+1, y+1]
-    return
+# def check(x, y):
+#     global answer
+#     if graph[x][y] == target:
+#         answer = [x+1, y+1]
+#     return
 
 dx = [-1, 0, 1, 0]  # 북 동 남 서
 dy = [0, 1, 0, -1]
 
 cnt = 2  # 반복 횟수 2배씩
-i = 1  # start value
+num = 1  # start value
 x, y = n//2, n//2  # (좌표)
 while True:
     if cnt > n:
         for _ in range(cnt-2):
-            i += 1
+            num += 1
             x, y = x+dx[0], y+dy[0]
-            graph[x][y] = i
-            check(x, y)
+            graph[x][y] = num
+            # check(x, y)
         break
             
     for _ in range(cnt-1):
-        i += 1
+        num += 1
         x, y = x+dx[0], y+dy[0]
-        graph[x][y] = i
-        check(x, y)
+        graph[x][y] = num
+        # check(x, y)
     
     for _ in range(cnt-1):
-        i += 1
+        num += 1
         x, y = x+dx[1], y+dy[1]
-        graph[x][y] = i
-        check(x, y)
+        graph[x][y] = num
+        # check(x, y)
     
     for _ in range(cnt):
-        i += 1
+        num += 1
         x, y = x+dx[2], y+dy[2]
-        graph[x][y] = i
-        check(x, y)
+        graph[x][y] = num
+        # check(x, y)
     
     for _ in range(cnt):
-        i += 1
+        num += 1
         x, y = x+dx[3], y+dy[3]
-        graph[x][y] = i
-        check(x, y)
+        graph[x][y] = num
+        # check(x, y)
     
     cnt += 2
 
-for row in graph:
-    print(*row)
+answer = [n//2, n//2]  # initialize 1!
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == target:
+            answer = i+1, j+1
+    print(*graph[i])
 
 print(*answer)
