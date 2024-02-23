47bcb39734b815505f1ae8f5074848a7c706f5b0
pimang62 <pimang62@gmail.com>
2024-02-23 14:02:15 +0900

    2/23

--- baekjoon/구현/14719.py
+++ baekjoon/구현/14719.py
@@ -8,29 +8,67 @@
 1 0 0 1 
 1 0 1 1
 
-3 0 1 4
+3 0 1 | 4 |
+3 3 3
+3 0 1
+  3 2 
 
-3 1 2 3 4 1 1 2
+3 1 2 3 | 4 | 1 1 2
+3 3 3 3       2 2 2 
+3 1 2 3       1 1 2
+  2 1 0       1 1 0
 
-ans, prev_max,  
-- 처음 기록된 값 <= 현재 값:
-    처음 기록된 값 = 현재값
-    ans += tmp
-    tmp = 0
-- 처음 기록된 값 > 현재 값:
-    tmp += (첫 - 현)
+- max값을 기준으로 왼쪽, 오른쪽에서 훑음
+- 각 구간에서의 (두 번째임을 보장할 수 있는 곳에서) max와 현재값을 비교
 
 '''
 h, w = map(int, input().split())
 
 A = list(map(int, input().split()))
 
-ans, prev, tmp = 0, 0, 0
-for i in range(w):
-    if prev <= A[i]:
-        
+# maxi, idx = max(A), A.index(maxi)
+idx, maxi = 0, 0
+for i in range(len(A)):
+    if A[i] > maxi:
+        maxi = A[i]
+        idx = i
 
-    else:  # prev > A[i]
-        tmp = max(tmp, A[i])
+# 투 포인터
+answer = 0
+left_max = 0
+for i in range(idx):
+    if A[i] > left_max:
+        left_max = A[i]
+    answer += left_max - A[i]
 
-print(ans)
+right_max = 0
+for j in range(w-1, idx-1, -1):
+    if A[j] > right_max:
+        right_max = A[j]
+    answer += right_max - A[j]
+
+print(answer)
+
+# 스택
+result = 0
+left_stack = []
+for i in range(idx):
+    if not left_stack:
+        left_stack.append(A[i])
+        continue
+    if left_stack[-1] < A[i]:
+        left_stack.pop()
+        left_stack.append(A[i])
+    result += left_stack[-1] - A[i]
+
+right_stack = []
+for j in range(w-1, idx-1, -1):
+    if not right_stack:
+        right_stack.append(A[j])
+        continue
+    if right_stack[-1] < A[j]:
+        right_stack.pop()
+        right_stack.append(A[j])
+    result += right_stack[-1] - A[j]
+
+print(result)
