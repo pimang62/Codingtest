86074eebc322f4f34684fc9107160f6f498913ed
pimang62 <pimang62@gmail.com>
2024-08-04 18:21:51 +0900

    7/30

---
+++ baekjoon/구현/14912.py
@@ -0,0 +1,19 @@
+'''
+[숫자 빈도수]
+1부터 n까지 특정 숫자의 빈도수?
+
+ex. n = 11
+1 2 3 4 5 6 7 8 9 10 11
+1은 1, 10, 11 -> 4번
+
+1 <= n <= 1e5
+0 <= d <= 9
+'''
+n, d = map(int, input().split())
+
+answer = 0
+for i in range(1, n+1):
+    str_i, str_d = str(i), str(d)
+    answer += str_i.count(str_d)
+
+print(answer)
